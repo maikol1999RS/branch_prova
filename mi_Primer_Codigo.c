521b472f63ccad1a36e393e9e2bb3e090e883efd
maikol1999RS <maikolrodriguezsoliz@gmail.com>
2020-12-03 12:03:13 +0100

    commit 3

--- mi_Primer_Codigo.c
+++ mi_Primer_Codigo.c
@@ -4,6 +4,14 @@
 
 int main(){
 
+<<<<<<< HEAD
+printf("hola mundo\n");
+printf("pueba de commits 1");
+
 printf("hola mundo");
+printf("proba 2 - aver si funciona");
+
+printf("hola mundo");
+printf("proba 3 - aver si funciona");
 
 }
