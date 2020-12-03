d04500e8b2ef907e4281943d3c933c9c7e8d77f9
maikol1999RS <maikolrodriguezsoliz@gmail.com>
2020-12-03 12:09:25 +0100

    merge commit

--- mi_Primer_Codigo.c
+++ mi_Primer_Codigo.c
@@ -3,19 +3,12 @@
 
 
 int main(){
+funcionalidadbasica();
 
-<<<<<<< HEAD
-printf("hola mundo\n");
-printf("pueba de commits 1");
+}
 
-printf("hola mundo");
-printf("proba 2 - aver si funciona");
+int funcionalidadbasica(){
 
-printf("hola mundo");
-printf("proba 3 - aver si funciona");
-
-
-printf("hola mundo");
-printf("proba 4 - aver si funciona");
+  printf("otra prueva - para merge commit\n", );
 
 }
