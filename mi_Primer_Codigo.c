42cf1536ecf52b4aa6ebb4f61cf1d007edf186f8
maikol1999RS <maikolrodriguezsoliz@gmail.com>
2020-12-03 12:36:32 +0100

    push en la rama original : master

--- mi_Primer_Codigo.c
+++ mi_Primer_Codigo.c
@@ -4,11 +4,16 @@
 
 int main(){
 funcionalidadbasica();
-
+int funcionalidadbasica_extendida();
 }
 
 int funcionalidadbasica(){
 
   printf("otra prueva - para merge commit\n", );
+
+}
+int funcionalidadbasica_extendida(){
+
+  printf("otra prueva - para merge commit - en expansión\n", );
 
 }
